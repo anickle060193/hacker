49436f1de9c3b541a4d6770cfbc06f8a7495dedc
Adam Nickle <anickle060193@gmail.com>
2025-01-03 15:35:37 -0600

    Improve radar.

--- src/components/Radar.tsx
+++ src/components/Radar.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { keyframes } from "@emotion/react";
-import styled from "@emotion/styled";
 
 import { Cell, CellProps } from "./Cell";
 
-const Circle = styled("div")(({ theme }) => ({
-  aspectRatio: 1,
-  position: "absolute",
-  left: "50%",
-  top: "50%",
-  transform: "translate( -50%, -50% )",
-  borderWidth: 2,
-  borderStyle: "solid",
-  borderColor: theme.colors.primary,
-  borderRadius: "50%",
-  opacity: 0.5,
-}));
+import { useRandomInterval } from "../hooks/useRandomInterval";
+
+const RING_RADII = [1.0, 0.75, 0.5, 0.25];
 
 const sweeperAnimation = keyframes({
   "0%": {
@@ -31,7 +21,7 @@ const sweeperAnimation = keyframes({
 
 const pingKeyFrames = keyframes({
   "0%": {
-    opacity: 1,
+    opacity: 0.8,
     filter: "blur( 0 )",
   },
   "100%": {
@@ -42,40 +32,81 @@ const pingKeyFrames = keyframes({
 
 const SWEEP_ANIMATION_DURATION = "10s";
 
-const Ping = styled("div")(({ theme }) => ({
-  position: "absolute",
-  transform: "translate( -50%, -50% )",
-  width: 8,
-  aspectRatio: 1,
-  backgroundColor: theme.colors.primary,
-  borderRadius: "50%",
-  opacity: 0,
-  animation: `${pingKeyFrames} ${SWEEP_ANIMATION_DURATION} linear infinite`,
-}));
-
 export const Radar: React.FC<CellProps> = ({ ...cellProps }) => {
-  const [pings] = React.useState(() =>
-    Array.from({ length: 4 }, () => {
-      const x = Math.random();
-      const y = Math.random();
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
+  const sweeperStyleRef = React.useRef<CSSStyleDeclaration | null>(null);
 
-      const normX = (x - 0.5) * 2;
-      const normY = (0.5 - y) * 2;
-      const angle = Math.atan2(normY, normX);
-      const delay = (2 * Math.PI - angle) / (2 * Math.PI);
-      console.log("DELAY:", normX, normY, angle * (180 / Math.PI));
-
-      return {
-        x: `${(x * 100).toFixed(2)}%`,
-        y: `${(y * 100).toFixed(2)}%`,
-        delay: `calc( ${SWEEP_ANIMATION_DURATION} * ${delay.toFixed(8)} )`,
-      };
-    })
+  const sweeperCallbackRef = React.useCallback(
+    (sweeper: HTMLElement | null) => {
+      if (!sweeper) {
+        sweeperStyleRef.current = null;
+      } else {
+        const style = window.getComputedStyle(sweeper);
+        sweeperStyleRef.current = style;
+      }
+    },
+    []
   );
+
+  const [pings, setPings] = React.useState<
+    { id: string; x: number; y: number }[]
+  >([]);
+
+  useRandomInterval(true, 200, 2000, () => {
+    if (!containerRef.current) {
+      return;
+    }
+
+    const width = containerRef.current.clientWidth;
+    const height = containerRef.current.clientHeight;
+
+    if (!sweeperStyleRef.current) {
+      return;
+    }
+
+    const transform = sweeperStyleRef.current.transform;
+    const match = /^matrix\((.*), (.*), (.*), (.*), (.*), (.*)\)$/.exec(
+      transform
+    );
+    if (!match) {
+      return;
+    }
+
+    const [a, b, c, d] = match.slice(1).map((s) => +s);
+
+    let rotation: number;
+    if (a !== 0 || b !== 0) {
+      const r = Math.sqrt(a * a + b * b);
+      rotation = b > 0 ? Math.acos(a / r) : -Math.acos(a / r);
+    } else if (c !== 0 || d !== 0) {
+      const s = Math.sqrt(c * c + d * d);
+      rotation = Math.PI / 2 - (d > 0 ? Math.acos(-c / s) : -Math.acos(c / s));
+    } else {
+      rotation = 0;
+    }
+
+    const rot = rotation + (2 * Math.PI) / 180;
+    const size = Math.min(width, height);
+    const h = Math.sqrt(2 * size * size);
+    const ratio = Math.random() * 0.75 + 0.25;
+    const hyp = h * ratio;
+    const x = hyp * Math.cos(rot) + width / 2;
+    const y = hyp * Math.sin(rot) + height / 2;
+
+    setPings((p) => [
+      ...p,
+      {
+        x: x,
+        y: y,
+        id: crypto.randomUUID(),
+      },
+    ]);
+  });
 
   return (
     <Cell {...cellProps}>
       <div
+        ref={containerRef}
         css={{
           width: "100%",
           height: "100%",
@@ -83,41 +114,29 @@ export const Radar: React.FC<CellProps> = ({ ...cellProps }) => {
           overflow: "hidden",
         }}
       >
-        <Circle
-          style={{
-            width: "125%",
-          }}
-        />
-        <Circle
-          style={{
-            width: "100%",
-          }}
-        />
-        <Circle
-          style={{
-            width: "75%",
-          }}
-        />
-        <Circle
-          style={{
-            width: "50%",
-          }}
-        />
-        <Circle
-          style={{
-            width: "25%",
-          }}
-        />
-        <Circle
-          css={(theme) => ({
-            backgroundColor: theme.colors.primary,
-            opacity: 1,
-          })}
-          style={{
-            width: 10,
-          }}
-        />
+        {RING_RADII.map((r) => (
+          <div
+            key={r}
+            css={(theme) => ({
+              aspectRatio: 1,
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              transform: "translate( -50%, -50% )",
+              borderWidth: 2,
+              borderStyle: "solid",
+              borderColor: theme.colors.primary,
+              borderRadius: "50%",
+              opacity: 0.5,
+            })}
+            style={{
+              minWidth: `${(r * 100).toFixed(2)}%`,
+              minHeight: `${(r * 100).toFixed(2)}%`,
+            }}
+          />
+        ))}
         <div
+          ref={sweeperCallbackRef}
           css={(theme) => ({
             position: "absolute",
             top: "calc( 50% - 1px )",
@@ -143,13 +162,25 @@ export const Radar: React.FC<CellProps> = ({ ...cellProps }) => {
             },
           })}
         />
-        {pings.map(({ x, y, delay }, i) => (
-          <Ping
-            key={i}
+        {pings.map(({ id, x, y }) => (
+          <div
+            key={id}
+            css={(theme) => ({
+              position: "absolute",
+              transform: "translate( -50%, -50% )",
+              width: 8,
+              aspectRatio: 1,
+              backgroundColor: theme.colors.primary,
+              borderRadius: "50%",
+              opacity: 0,
+              animation: `${pingKeyFrames} ${SWEEP_ANIMATION_DURATION} linear`,
+            })}
             style={{
-              left: x,
-              top: y,
-              animationDelay: delay,
+              left: `${x.toFixed(2)}px`,
+              top: `${y.toFixed(2)}px`,
+            }}
+            onAnimationEnd={() => {
+              setPings((ps) => ps.filter((p) => p.id !== id));
             }}
           />
         ))}
